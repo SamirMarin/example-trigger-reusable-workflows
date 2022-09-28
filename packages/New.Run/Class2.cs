7106ab2a263458ff140d58f3a77c232c4d73cb10
Samir Marin <marin.samir@gmail.com>
2022-09-28 12:22:39 -0700

    new package

---
+++ packages/New.Run/Class2.cs
@@ -0,0 +1,10 @@
+// Hello Universe! program
+namespace New.Run
+{
+    class Class2 {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Universe!");
+        }
+    }
+}
